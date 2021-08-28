d4df3268f7121fc0a72ec3e312b71bc8382ea6f3
Rodney Rizo <rodneyrizo@outlook.com>
2021-08-27 23:08:36 -0600

    let's play!

--- src/juegoAzar/DataArray.java
+++ src/juegoAzar/DataArray.java
@@ -4,6 +4,7 @@ package juegoAzar;
  *
  * @author Rodney Rizo
  */
+//Clase constructora para ir almacenando posición por posición en el array
 public class DataArray {
     
     String [] array = new String[10];
